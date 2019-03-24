be0f7a53a288df159ccad0e088df053908109cfe
Jasper van Merle <jaspervmerle@gmail.com>
2019-03-24 02:11:33 +0100

    Add visibility toggling

--- src/components/containers/Box.tsx
+++ src/components/containers/Box.tsx
@@ -1,8 +1,12 @@
 import { Component, h } from 'hyperapp';
+import { Actions } from '../../app/actions';
+import { State } from '../../app/state';
 
-export const Box: Component = ({}, children) => (
-  <div class="gwc-box">
-    <span className="gwc-close-btn">✕</span>
+export const Box: Component<{}, State, Actions> = ({}, children) => (state: State, actions: Actions) => (
+  <div class={`gwc-box${state.visible ? '' : ' gwc-box-hidden'}`}>
+    <span className="gwc-close-btn" onclick={() => actions.setVisible(false)}>
+      ✕
+    </span>
 
     {children}
   </div>
